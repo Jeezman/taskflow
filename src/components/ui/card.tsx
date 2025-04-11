de170ea07b3bfb5e4b1ea499f00207288d88e0c1
Jeezman <tobiadeyemi91@gmail.com>
2025-04-11 08:33:40 +0100

    add simple card component

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/lib/utils';
+
+type CardProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function Card({ className, ...props }: CardProps) {
+  return (
+    <div
+      className={cn(
+        'rounded-lg border border-slate-200 bg-white shadow-sm',
+        className
+      )}
+      {...props}
+    />
+  );
+}
